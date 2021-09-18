d919401706a0c8b683eb714607d16c68a287617e
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-17 20:55:45 -0700

    simplified nvim installation

--- nvim.sh
+++ nvim.sh
@@ -15,17 +15,3 @@ sudo apt install neovim -y
 # Configure Neovim to use .vimrc file
 mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.vim
-
-# Tell 
-
-# Let NVIM and VIM share configuration files
-echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
-echo "let &packpath=&runtimepath" >> ~/.config/nvim/init.vim
-echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
-
-# Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# Move .vimrc file to correct location
-mv ./reference/.vimrc ~/.vimrc
