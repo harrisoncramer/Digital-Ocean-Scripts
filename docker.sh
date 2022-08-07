77c0883033f9bdef9b6b750c62165ec13d140141
Harrison Cramer <harrisoncramer@gmail.com>
2022-08-06 23:47:19 -0400

    Added note about non-root group permissions

--- docker.sh
+++ docker.sh
@@ -28,3 +28,11 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+
+echo << EOF
+  Successfully installed docker and docker-compose.
+  Please add your user to the docker group to enable running commands as non-root:
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+EOF
