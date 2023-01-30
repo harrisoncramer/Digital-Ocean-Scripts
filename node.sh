3f5312b622482d301c0622ca61a39fe5d23dd758
Harrison (Harry) Cramer <32515581+harrisoncramer@users.noreply.github.com>
2023-01-30 14:58:23 -0500

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Installs NVM and adds the sourcing to our .zshrc file
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # Sources it...
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 13.7.0 # Installs version of node.
+nvm install 18 # Installs version of node.
 
 echo "Source the new installation w/ ~/.zshrc"
